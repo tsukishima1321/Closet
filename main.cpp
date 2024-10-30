a56e5dd60075adc31ef1f19f2be2526623b3a7ef
tsukishima1321 <18001751830@163.com>
2024-10-30 10:39:19 +0800

    update

--- main.cpp
+++ main.cpp
@@ -1,9 +1,21 @@
 #include "welcome.h"
-#include <QMessageBox>
 #include <QApplication>
+#include <QDir>
+#include <QLockFile>
+#include <QMessageBox>
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
+    QString path = QDir::temp().absoluteFilePath("SingleApp.lock");
+    QLockFile lockFile(path);
+
+    bool isLock = lockFile.isLocked();
+    (void)isLock;
+    if (!lockFile.tryLock(100)) {
+        QMessageBox::warning(NULL, "提示", "程序已在运行中");
+        return 0;
+    }
+
     QGuiApplication::setHighDpiScaleFactorRoundingPolicy(Qt::HighDpiScaleFactorRoundingPolicy::Floor);
     Welcome w;
     w.show();
